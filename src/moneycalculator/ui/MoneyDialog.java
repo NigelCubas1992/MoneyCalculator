118e3a383254abfe2d913027a43d323e84829dab
NigelCubas1992 <NigelCubas1992@users.noreply.github.com>
2021-01-30 15:20:44 +0000

    project created

---
+++ src/moneycalculator/ui/MoneyDialog.java
@@ -0,0 +1,12 @@
+package moneycalculator.ui;
+
+import moneycalculator.model.Money;
+
+
+public interface MoneyDialog {
+    
+    Money get();
+    
+}
+
+
---
+++ src/moneycalculator/ui/MoneyDisplay.java
@@ -0,0 +1,11 @@
+package moneycalculator.ui;
+
+import moneycalculator.model.Money;
+
+public interface MoneyDisplay {
+    
+    void display(Money money);
+    
+}
+
+
